bb0c7925db42145b89b50f85b557ca681cb68827
unknown <holupkern@gmail.com>
2023-07-24 16:20:58 +0800

    first commit - practice basic react router dom to achieve same page navigation

---
+++ src/pages/Posts.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Posts(){
+    return(
+        <React.Fragment>
+            <h1>Posts</h1>
+        </React.Fragment>
+    )
+}
---
+++ src/pages/SubmittedForm.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function SubmittedForm(){
+    return(
+        <React.Fragment>
+            <h1> Submitted Form </h1>
+        </React.Fragment>
+    )
+}
